d3c5a132511bc089841fbe5e9099f779b424f8b4
Doll Milan <ryabink.2000@icloud.com>
2021-03-14 13:53:15 +0300

    draft on wordpress and mysql

--- srcs/wordpress/setup.sh
+++ srcs/wordpress/setup.sh
@@ -1,5 +1,2 @@
-
-
-/usr/bin/mysql_install_db --user=mysql
-/etc/init.d/mariadb start && rc-update add mariadb default
-/usr/bin/mysqladmin -u root password 'password'
+# php-fpm -F -R
+nginx -g "daemon off;"
--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -23,13 +23,13 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'wordpress' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'password' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mysql' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
