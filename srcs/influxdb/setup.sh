cad49ad57b3ead3be48f940569d9447ac20ba74e
Doll Milan <ryabink.2000@icloud.com>
2021-03-30 10:55:19 +0300

    fixed telegraf urls, now metrics can be shown

--- srcs/influxdb/setup.sh
+++ srcs/influxdb/setup.sh
@@ -1,3 +1,5 @@
-influxd &;
+influxd & sleep 5;
 
 influx -execute "CREATE DATABASE telegraf; CREATE USER telegraf WITH PASSWORD 'password'"
+
+/usr/bin/supervisord
--- srcs/mysql/mysql_setup.sh
+++ srcs/mysql/mysql_setup.sh
@@ -1,6 +1,6 @@
 mysql_install_db --datadir=/var/lib/mysql;
 
-mysqld --user=root & sleep 5;
+mysqld & sleep 5;
 
 # mysql -u root mysql < /create_wordpress_db.sql;
 mysql < /wordpress.sql;
